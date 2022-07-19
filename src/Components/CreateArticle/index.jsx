4ce431772c6fee6b27247d733bb764b4742c03d2
NishithDG <108052807+NishithDG@users.noreply.github.com>
2022-07-19 18:45:41 +0530

    upload image on cloud

--- src/Components/CreateArticle/index.jsx
+++ src/Components/CreateArticle/index.jsx
@@ -10,7 +10,7 @@ class CreateArticle extends React.Component{
         image:null,
         content:'',
         channel:null,
-        errors:{},
+        errors:[],
         categories:[],          
       }
     }
@@ -23,11 +23,23 @@ class CreateArticle extends React.Component{
         
     }
 
+    handleSubmit = async(event)=>{
+        console.log('submitting form')
+        event.preventDefault()
+        try{
+            const article = await this.props.createArticle(this.state,this.props.token)
+            this.props.history.push("/")
+        }catch(errors){
+            this.setState({errors})
+        }
+    }
+
     handleInputChange = (event) => {
         this.setState({
-            [event.target.name] : event.target.type == 'files' ? event.taget.files[0] : event.target.value
+            [event.target.name] : (event.target.type === 'file' ? event.target.files[0] : event.target.value)           
         })
         console.log(this.state)
+        console.log('updating inputs')
     }
 
     render(){
@@ -35,11 +47,18 @@ class CreateArticle extends React.Component{
             <CreateArticleForm
             handleInputChange={this.handleInputChange}
             categories = {this.state.categories}
+            handleSubmit = {this.handleSubmit}
+            errors = {this.state.errors}
             />
         )
     }
 }
 CreateArticle.propTypes = {
     getArticleCategories: PropTypes.func.isRequired,
+    createArticle:PropTypes.func.isRequired,
+    token:PropTypes.string.isRequired,
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired,
+      }).isRequired,
 }
 export default CreateArticle;
